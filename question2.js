2558ed141c4a444807c75c528481236eac9ce8e8
mohammedfathaah <mohammedfathaah@gmail.com>
2025-07-18 09:49:05 +0530

    first questiona and second question completed

---
+++ question2.js
@@ -0,0 +1,16 @@
+let num=15;
+if(num%3===0){
+    if(num%5===0){
+        console.log("fizzbuzz")
+    }
+    else{
+        console.log("fizz");
+    }
+}
+    else {if(num%5===0){
+        console.log("buzz")
+    }
+    else{
+        console.log("not fizz buzz")
+    }
+}
