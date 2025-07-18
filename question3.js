fca6997365db7d298ef063eaafb761ac746f82d4
mohammedfathaah <mohammedfathaah@gmail.com>
2025-07-18 10:37:44 +0530

    upto question4 completed

---
+++ question3.js
@@ -0,0 +1,10 @@
+let age = 45;
+if (age < 13) {
+    console.log("Child");
+} else if (age >= 13 && age <= 19) {
+    console.log("Teen");
+} else if (age >= 20 && age <= 60) {
+    console.log("Adult");
+} else {
+    console.log("Senior");
+}
---
+++ question4.js
@@ -0,0 +1,18 @@
+let vehicleType = "b";
+
+switch (vehicleType) {
+    case "c":
+        console.log("Car");
+        break;
+    case "b":
+        console.log("Bike");
+        break;
+    case "t":
+        console.log("Truck");
+        break;
+    case "a":
+        console.log("Auto");
+        break;
+    default:
+        console.log("Unknown Vehicle");
+}
